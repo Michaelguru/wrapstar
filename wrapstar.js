f3d39302d699ba5343f7fbc303073f6dccf84907
Michaelguru <161510080+Michaelguru@users.noreply.github.com>
2024-12-05 22:55:06 +0100

    Add files via upload

--- wrapstar.js
+++ wrapstar.js
@@ -1,3 +1,29 @@
+
+const structuredData = {
+    "@context": "https://schema.org",
+    "type": "Organization",
+    "name": "Wrapstar",
+    "url": "https://wrapstar.com.ng",
+    "logo": "https://wrapstar.com.ng/images/Wrapstar logo.png"
+};
+
+const scriptTag = document.createElement("script");
+scriptTag.type = "application/ld+json";
+scriptTag.textContent = JSON.stringify(structuredData);
+
+document.head.appendChild(scriptTag);
+
+
+
+
+
+
+
+
+
+
+
+
 window.onload = function(){
     document.querySelector('.loader').style.display = 'none';
 };
@@ -28,3 +54,15 @@ function Invalid(){
 document.getElementById('orderNow').addEventListener("click", function(event){
     event.preventDefault();
 })
+
+function showMap(){
+    document.getElementById("map").style.display = 'unset';
+    document.getElementById("showMap").style.display = 'none';
+    document.getElementById("hideMap").style.display = 'unset';
+};
+
+function hideMap(){
+    document.getElementById("map").style.display = 'none';
+    document.getElementById("showMap").style.display = 'unset';
+    document.getElementById("hideMap").style.display = 'none';
+};
