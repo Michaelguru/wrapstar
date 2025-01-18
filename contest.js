3d1978d5d7c607d6b06fe85a1a62f49cf4b24d2a
Michaelguru <161510080+Michaelguru@users.noreply.github.com>
2025-01-18 16:33:21 +0100

    Add files via upload

---
+++ contest.js
@@ -0,0 +1,30 @@
+
+
+function checkFollow(platform) {
+    if (platform === 'instagram') {
+        document.getElementById('instagramFollow').checked = true;
+    } else if (platform === 'tiktok') {
+        document.getElementById('tiktokFollow').checked = true;
+    }
+}
+
+document.getElementById('wrapstarForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const menuWords = document.getElementById('Words').value.trim();
+    if (!menuWords) {
+        alert('Please fill out the text area!');
+        return;
+    }
+
+    const instagramFollowed = document.getElementById('instagramFollow').checked;
+    const tiktokFollowed = document.getElementById('tiktokFollow').checked;
+    if (!instagramFollowed || !tiktokFollowed){
+        alert('Please follow wrapstar on both Instagram and Tiktok to continue!');
+        return;
+    }
+
+    const whatsappNumber = '09157684983';
+    const whatsappMessage = `Here are the words I found: ${menuWords}`;
+    const whatsappURL = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(whatsappMessage)}`;
+    window.open(whatsappURL, '_blank');
+});
