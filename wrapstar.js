2908fb5ded876f6eed695062ba0290b7d095878f
Michaelguru <161510080+Michaelguru@users.noreply.github.com>
2025-02-01 16:42:45 +0100

    Add files via upload

--- wrapstar.js
+++ wrapstar.js
@@ -24,12 +24,9 @@ document.head.appendChild(scriptTag);
 
 
 
-window.onload = function(){
-    document.querySelector('.loader').style.display = 'none';
-};
-
 document.addEventListener("DOMContentLoaded", function(){
     const sections = document.querySelectorAll('.section');
+    const alert = document.getElementById("couplesComboAlert");
     const options = {
         threshold: 0.1
     };
@@ -44,16 +41,34 @@ document.addEventListener("DOMContentLoaded", function(){
     sections.forEach(section => {
         observer.observe(section);
     });
+
+    //observe the couple combo alert
+    if (alert) {
+        observer.observe(alert);
+    }
 });
 
 
-function Invalid(){
-    document.getElementById("InvalidCoupleCombo").innerText= "Sorry! No Couple Combo Yet.";
-    
+function dismissAlert(){
+    document.getElementById("couplesComboAlert").classList.remove('visible');
+    sessionStorage.setItem("couplesComboDismissed", "true");
 }
-document.getElementById('orderNow').addEventListener("click", function(event){
-    event.preventDefault();
-})
+
+window.onload = function(){
+    document.querySelector('.loader').style.display = 'none';
+    if (sessionStorage.getItem("couplesCombismissed")) {
+        document.getElementById("couplesComboAlert").style.display = "none";
+    }
+}
+
+
+// function Invalid(){
+//     document.getElementById("InvalidCoupleCombo").innerText= "Sorry! No Couple Combo Yet.";
+    
+// }
+// document.getElementById('orderNow').addEventListener("click", function(event){
+//     event.preventDefault();
+// })
 
 // function showMap(){
 //     document.getElementById("map").style.display = 'unset';
